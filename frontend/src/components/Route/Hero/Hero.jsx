424c04ed47c23343022a3a73bf7628f059d431a7
bright <obololastkiller@gmail.com>
2023-04-14 05:06:50 +0000

    Categories page updatted

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import styles from "../../../styles/styles";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <div
-      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.normalFlex}`}
+      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-50% bg-no-repeat ${styles.normalFlex}`}
       style={{
         backgroundImage:
-          "url(https://thumbs.dreamstime.com/b/may-africa-day-banner-animal-print-map-illustration-celebration-african-continent-ethnic-art-wild-textures-147521780.jpg)",
+          "url(https://sothebys-com.brightspotcdn.com/dims4/default/c4d4d2f/2147483647/strip/true/crop/5062x1775+0+0/resize/2000x701!/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2F39%2F99%2F8ba5bb19492ea00d6fc627bb7e39%2Fhk1219-marketing-banner.jpg)",
       }}
-    ></div>
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+        <h1
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+        >
+          Best Collection <br /> Around Africa
+        </h1>
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
+          Overall, the artefacts of Africa are a testament to the rich cultural
+          <br />
+          history of the continent and continue to be important to many <br />
+          African communities today.
+        </p>
+        <Link to="/products" className="inline-block">
+          <div className={`${styles.button} mt-5`}>
+            <span className="text-[#ffffff] font-[Poppins] text-[18px]">
+              Shop Now
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 };
 
